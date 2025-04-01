76e316544d8c6d9bfeabb7f85b3b8d77320c32dd
Thien Dev <nguyenvthiendev@gmail.com>
2025-04-01 15:22:54 -0400

    update

--- src/components/Layout/Header/Navigation/menuData.tsx
+++ src/components/Layout/Header/Navigation/menuData.tsx
@@ -1,9 +1,9 @@
 import { HeaderItem } from "@/types/menu";
 
 export const headerData: HeaderItem[] = [
-  { label: "Trang Chủ", href: "#home-section" },
+  { label: "Trang Chủ", href: "/#home-section" },
   { label: "Liên Hệ", href: "/contact-us" },
-  { label: "Chuyên Khoa", href: "#specialty-section" },
+  { label: "Chuyên Khoa", href: "/#specialty-section" },
   { label: "Về Chúng Tôi", href: "/about-us" },
-  { label: "Quy Trình Khám", href: "#process-section" },
+  { label: "Quy Trình Khám", href: "/#process-section" },
 ];
