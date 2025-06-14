b5911bc3065e59445be511885dc046f130f55cae
Jye_a_dev <shokoste1@gmail.com>
2025-06-14 23:11:05 +0700

    Delete jyes-portfolio directory

--- jyes-portfolio/src/libs/formatDate.ts
+++
@@ -1,8 +0,0 @@
-// src/libs/formatDate.ts
-export const formatDate = (date: Date, locale = 'en-US'): string => {
-  return date.toLocaleDateString(locale, {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-  });
-};
