d25dd59b354e672c9e9e2bc3b049c0c6c68ef0ce
Jye-a-dev <shokoste1@gmail.com>
2025-09-09 22:07:34 +0700

    chore: remove jyes_profile folder

--- jyes_profile/src/components/layout/Footer.tsx
+++
@@ -1,8 +0,0 @@
-// components/layout/Footer.tsx
-export default function Footer() {
-  return (
-    <footer className="w-full text-center py-2 bg-red-400 text-white shadow-sm border-2 border-red-500">
-      © {new Date().getFullYear()} Jye's Profile
-    </footer>
-  )
-}
