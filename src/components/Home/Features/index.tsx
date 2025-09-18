760b8388d83b0e01ff41a479974c0e9aa08e1ab4
Thien DEV <thien@gkim.vn>
2025-09-18 08:00:42 +0700

    feat: enhance configuration and add new components for improved functionality

--- src/components/Home/Features/index.tsx
+++ src/components/Home/Features/index.tsx
@@ -15,7 +15,7 @@ const Features = () => {
         </div>
 
         {/* Render items thẳng ra */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-6 gap-6 p-20 ">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-6 gap-6">
           {FeaturesData.map((item, i) => (
             <div 
               key={i} 
